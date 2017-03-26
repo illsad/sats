81c8b7577a1dff8d254b98bdd382823a90e8f9d4
achyar1016 <achyar.anshorie@gmail.com>
2017-03-26 07:48:07 +0700

    design login

--- application/views/class/login.php
+++ application/views/class/login.php
@@ -1,50 +1,101 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>SATS | Login</title>
-        <link rel="icon" href="<?php echo media_url('ico/favicon.png'); ?>" type="image/x-icon">
-        <link rel="stylesheet" href="<?php echo media_url() ?>/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo media_url() ?>/css/login.css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-4 col-md-offset-4">
-                    <h1 class="text-center login-title">Masuk ke halaman kelas</h1>
-                    <div class="account-wall">
-                        <img class="profile-img" src="<?php echo media_url() ?>/images/user.png"
-                             alt="">
-                        <form class="form-signin" role="form" action="<?php echo site_url('class/auth/login') ?>" method="post">
-                            <?php
-                            echo form_open(current_url(), array('role' => 'form', 'class' => 'form-signin'));
-                            if (isset($_GET['location'])) {
-                                echo '<input type="hidden" name="location" value="';
-                                if (isset($_GET['location'])) {
-                                    echo htmlspecialchars($_GET['location']);
-                                }
-                                echo '" />';
-                            }
-                            ?>
-                            <?php if ($this->session->flashdata('failed')) { ?>
-                                <div class="callout callout-danger">
-                                    <h5><i class="glyphicon glyphicon-warning-sign"></i> Username atau password salah!</h5>
 
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>SATS Login | Kelas</title> 
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo media_url('ico/favicon.jpg'); ?>" type="image/x-icon">
+    <!-- CSS Style -->
+    <link href="<?php echo media_url() ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo media_url() ?>/css/design.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo media_url() ?>/css/load-font.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo media_url() ?>/fonts/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Javascript -->
+    
+    <script src="<?php echo media_url() ?>/js/jquery-3.2.0.min.js"></script>
+    <script src="<?php echo media_url() ?>/js/jquery-migrate-3.0.0.min.js"></script>
+    <script src="<?php echo media_url() ?>/js/jquery-ui.min.js"></script>
+    
+    <script src="<?php echo media_url() ?>/js/design.js"></script>
+    <script src="<?php echo media_url() ?>/js/design.min.js"></script>
+    <!-- Theme initialization --> 
+    
+</head>
+
+<body>
+    <nav class="navbar navbar-primary navbar-transparent navbar-absolute">
+        <div class="container">
+            <!-- <div class="navbar-header">
+                <a class="navbar-brand" href=" #"><img src="<?php echo media_url() ?>/img/back.png" height="100px" width="260px"></a>
+            </div> -->
+        </div>
+    </nav>
+    <div class="wrapper wrapper-full-page">
+        <div class="full-page login-page" filter-color="black" data-image="<?php echo media_url() ?>/img/lock.jpeg">
+            <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
+                            <div class="card card-login">
+                                <div class="card-header text-center" data-background-color="orange">
+                                    <h4 class="card-title">SMK Wirabuana</h4>       
+                                </div>
+                                <p class="category text-center">
+                                    Dear user login here!
+                                </p>
+                                <div class="card-content">
+                                    <?php echo form_open('admin/auth/login'); ?>
+                                    <?php
+                                    if (isset($_GET['location'])) {
+                                        echo '<input type="hidden" name="location" value="';
+                                        if (isset($_GET['location'])) {
+                                            echo htmlspecialchars($_GET['location']);
+                                        }
+                                        echo '" />';
+                                    } ?>
+                                    <!-- Jika Error -->
+                                    <?php if ($this->session->flashdata('failed')) { ?>
+                                    <div class="danger">
+                                        <center><p><i class="material-icons text-danger">warning</i> <?php echo $this->session->flashdata('failed') ?></p></center>
                                     </div>
-                            <?php } ?>
-                            <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
-                            <input type="password" name="password" class="form-control" placeholder="Password" required>
-                            <button class="btn btn-lg btn-primary btn-block" type="submit">
-                                Sign in</button>
-                        </form>
+                                    <?php } ?>
+                                    <div class="input-group">
+                                        <span class="input-group-addon">
+                                            <i class="material-icons">face</i>
+                                        </span>
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Username</label>
+                                            <input type="text" autofocus name="username" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="input-group">
+                                        <span class="input-group-addon">
+                                            <i class="material-icons">lock_outline</i>
+                                        </span>
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Password</label>
+                                            <input type="password" name="password" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="footer text-center">
+                                    <button type="submit" class="btn btn-warning btn-wd btn-lg">Login</button>  
+                                </div>
+                            </div>
+                            <?php echo form_close(); ?>
+                            <div class="text-xs-center">
+                            <center><span class="btn btn-warning rounded btn-sm">Copyright &copy; <script>document.write(new Date().getFullYear())</script> All rights reserved</span>
+                            </div></center>
+                        </div>
                     </div>
-                </div>
-            </div><br>
-            <div class="row>">
-                <div class="col-md-12">
-                    <center><strong>Copyright &copy; 2016 - Sistem Absensi Terintegrasi Sekolah.</strong></center>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>

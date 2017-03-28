78221b79d2c074853d9628e0cf52e08b4ace979e
illsad <ilfansaid@gmail.com>
2017-03-29 00:20:13 +0700

    tambah tgl dan jam

--- application/views/class/layout.php
+++ application/views/class/layout.php
@@ -79,6 +79,7 @@
             </div>
 
             <ul class="nav navbar-nav navbar-right">
+
               <li class="">
                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                   <img src="<?php echo media_url() ?>/images/user.png" alt=""><?php echo $text = $this->session->userdata('class_level'). ' '.ucfirst($this->session->userdata('class_name')); ?>
@@ -98,6 +99,10 @@
                     </center>
                   </li>
                 </ul>
+              </li>
+
+              <li class="">
+               <h2 id="date-time"></h2>
               </li>
 
 
@@ -194,6 +199,73 @@
               text: '<?php echo $this->session->flashdata('success') ?>'
             });
           });
+
+          
+
+            function updatingClock(selector, type) {
+    function currentDate() {
+        var currentDate = new Date;
+        var Day = currentDate.getDate();
+        if (Day < 10) {
+            Day = '0' + Day;
+        } //end if
+        var Month = currentDate.getMonth() + 1;
+        if (Month < 10) {
+            Month = '0' + Month;
+        } //end if
+        var Year = currentDate.getFullYear();
+        var fullDate = Month + '/' + Day + '/' + Year;
+        return fullDate;
+    } //end current date function
+
+    function currentTime() {
+        var currentTime = new Date;
+        var Minutes = currentTime.getMinutes();
+        if (Minutes < 10) {
+            Minutes = '0' + Minutes;
+        }
+        var Hour = currentTime.getHours();
+        if (Hour > 12) {
+            Hour -= 12;
+        } //end if
+        var Time = Hour + ':' + Minutes;
+        if (currentTime.getHours() <= 12) {
+            Time += ' AM';
+        } //end if
+        if (currentTime.getHours() > 12) {
+            Time += ' PM';
+        } //end if
+        return Time;
+    } // end current time function
+
+
+    function updateOutput() {
+        var output;
+        if (type == 'time') {
+            output = currentTime();
+            if ($(selector).text() != output) {
+                $(selector).text(output);
+            } //end if
+        } //end if
+        if (type == 'date') {
+            output = currentDate();
+            if ($(selector).text() != output) {
+                $(selector).text(output);
+            } //end if
+        } //end if
+        if (type == 'both') {
+            output = currentDate() + ' at ' + currentTime();
+            if ($(selector).text() != output) {
+                $(selector).text(output);
+            } //end if
+        } //end if
+    }//end update output function
+    updateOutput();
+    window.setInterval(function() {
+        updateOutput();
+    }, 1000); //run update every 1 second
+} // end updating clock function
+updatingClock('#date-time', 'both');
         </script>
         <?php } ?>
       </body>

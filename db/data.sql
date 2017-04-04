e1671137d318bae9a72e6e6090baaa43bb18b6c9
Sistiandy <sistiandy@artikulpi.com>
2017-04-04 10:26:59 +0700

    Perbaikan teacher dan tambahan data

--- db/data.sql
+++ db/data.sql
@@ -12,5 +12,6 @@ INSERT INTO `user_role` (`role_id`, `role_name`) VALUES
 --
 
 INSERT INTO `user` (`user_id`, `user_name`, `user_full_name`, `user_password`, `user_email`, `user_description`, `user_input_date`, `user_last_update`, `user_role_role_id`, `user_is_deleted`) VALUES
-(1, 'admin', 'Admin', 'cfae66c98aa8d86383e07f1e1ea5d68e1cc6a613', 'admin@example.com', 'Admin default', '2015-07-30 04:32:54', '2015-07-30 04:32:54', 1, 0);
+(1, 'admin', 'Admin', 'cfae66c98aa8d86383e07f1e1ea5d68e1cc6a613', 'admin@example.com', 'Admin default', '2015-07-30 04:32:54', '2015-07-30 04:32:54', 1, 0),
+(1, 'ilfan', 'Ilfan Said', 'cfae66c98aa8d86383e07f1e1ea5d68e1cc6a613', 'ilfan@example.com', 'Admin default', '2015-07-30 04:32:54', '2015-07-30 04:32:54', 1, 0);
 
